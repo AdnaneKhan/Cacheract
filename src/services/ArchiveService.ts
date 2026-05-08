56bb4ac485ebaa3842fbeb42fba9cf0b4dcd0652
AdnaneKhan <adnanekhan@users.noreply.github.com>
2026-05-07 21:43:57 -0400

    fix build issue

--- src/services/ArchiveService.ts
+++ src/services/ArchiveService.ts
@@ -34,8 +34,8 @@ export class ArchiveService {
             }
             writeStream.end();
 
-            await new Promise((resolve, reject) => {
-                writeStream.on('finish', resolve);
+            await new Promise<void>((resolve, reject) => {
+                writeStream.on('finish', () => resolve());
                 writeStream.on('error', reject);
             });
         } catch (error) {
